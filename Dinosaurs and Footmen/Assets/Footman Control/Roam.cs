5af9bb94c03643938c0794076bc6d63a804b482c
mihaiandrei1294 <mihai_andrei_12@yahoo.com>
2016-10-24 19:23:24 +0200

    increased footmen speed

--- Dinosaurs and Footmen/Assets/Footman Control/Roam.cs
+++ Dinosaurs and Footmen/Assets/Footman Control/Roam.cs
@@ -13,6 +13,7 @@ public class Roam : MonoBehaviour {
 
 		biped = GetComponent<Biped> ();
 		biped.enabled = true;
+		biped.MaxSpeed = 2;
 
 		steerForPoint = GetComponent<SteerForPoint>();
 		steerForPoint.enabled = true;
@@ -29,7 +30,7 @@ public class Roam : MonoBehaviour {
 			steerForPoint.TargetPoint = generateRandomTargetPoint(new Vector2(160.0f, 280.0f),new Vector2(180.0f, 200.0f));
 			control.Run ();
 
-			biped.MaxSpeed = 4;
+			biped.MaxSpeed = 6;
 
 
 		}
