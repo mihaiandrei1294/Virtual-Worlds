d0af748fc544c30c03b0200de1e9298d5bae9442
mguy02 <zelordking@gmail.com>
2017-01-01 14:01:57 +0100

    made some spots for the staff of pain. Now, on start, the staff of pain will be put randomly to one of these spots

---
+++ Dinosaurs and Footmen/Assets/StaffControl/StaffControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaffControl : MonoBehaviour {
+
+	public Vector3[] startPositions;	//array of possible starting positions of the staff of pain
+	
+	private Vector3 defaultPosition = new Vector3(271, 16, 326);	//just a default position in case no position has been set
+	
+	// Use this for initialization
+	void Start () {
+		if(startPositions.Length == 0)
+		{
+			this.transform.position = defaultPosition;
+		}
+		else
+		{
+			this.transform.position = startPositions[Random.Range(0, startPositions.Length)];
+		}
+		
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
