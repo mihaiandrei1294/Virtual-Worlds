89a82ed60068c4ea3baeb4bcde04c0f60847536f
mihaiandrei1294 <mihai_andrei_12@yahoo.com>
2017-01-08 11:57:19 +0200

    Fixed the problem with the footmen runnong after the skeleton (when scaling the flee vector it could go beloe 0 so then they would go towards the skeleton instead of running from it)

--- Dinosaurs and Footmen/Assets/Footman Control/FleeAction.cs
+++ Dinosaurs and Footmen/Assets/Footman Control/FleeAction.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FleeAction : MonoBehaviour {
+public class FleeAction : MonoBehaviour
+{
 
-	private FootmanControl parent; //main script that will have useful variables, used to handle animations
+	private FootmanControl parent;
+	//main script that will have useful variables, used to handle animations
 	private GameObject skeleton;
 	
 	private NavMeshAgent agent;
@@ -12,20 +14,24 @@ public class FleeAction : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-		parent = GetComponent<FootmanControl>();
-		agent = GetComponent<NavMeshAgent>();
-		skeleton = GameObject.FindWithTag("skeleton");
+		parent = GetComponent<FootmanControl> ();
+		agent = GetComponent<NavMeshAgent> ();
+		skeleton = GameObject.FindWithTag ("skeleton");
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		Vector3 fleeVector = gameObject.transform.position - skeleton.transform.position;	//= - (skelPos - footPos)
-		fleeVector =  fleeVector * (1 + parent.rangeView() - Vector3.Distance(this.transform.position, skeleton.transform.position));	//the closer the skeleton, the more we run away and forget about our target
+		Vector3 fleeVector = this.transform.position - skeleton.transform.position;	//= - (skelPos - footPos)
+//		Debug.Log ("flee vector scaling is: " + (1 + parent.rangeView () - Vector3.Distance (this.transform.position, skeleton.transform.position)));
+		float scale = (1 + parent.rangeView () - Vector3.Distance (this.transform.position, skeleton.transform.position));
+		if (scale < 0)
+			scale = 0;
+		fleeVector = fleeVector * scale;	//the closer the skeleton, the more we run away and forget about our target
 		
-		Vector3 targetVector = parent.target.transform.position - gameObject.transform.position;
+		Vector3 targetVector = parent.target.transform.position - this.transform.position;
 		
-		Vector3 fleePoint = gameObject.transform.position + fleeVector +targetVector;		// = footPos + fleeVector
+		Vector3 fleePoint = gameObject.transform.position + fleeVector + targetVector;		// = footPos + fleeVector
 		
 		//This fleePoint can be on an obstacle. This can causes lag for SetDestination method to finish as it takes time to figure out a path is impossible.
 		//In order to solve that, we first do a RayCast to the target and 
@@ -34,22 +40,25 @@ public class FleeAction : MonoBehaviour {
 		
 		int numBlock = 1;
 		
-		do
-		{
+		do {
 			numBlock++;
-			blocked = NavMesh.Raycast(fleePoint - new Vector3(5f, 0f, 5f),fleePoint, out hit, NavMesh.AllAreas);
-			//Debug.DrawLine(gameObject.transform.position, fleePoint, blocked ? Color.red : Color.green);
-			if (blocked)
-			{
-				//Debug.DrawRay(hit.position, Vector3.up*1000, Color.red);
-				fleePoint = fleePoint - new Vector3(5f, 0f, 5f);
+			blocked = NavMesh.Raycast (fleePoint - new Vector3 (5f, 0f, 5f), fleePoint, out hit, NavMesh.AllAreas);
+//			Debug.DrawLine (gameObject.transform.position, fleePoint, blocked ? Color.red : Color.green);
+			if (blocked) {
+//				Debug.DrawRay (hit.position, Vector3.up * 1000, Color.red);
+				fleePoint = fleePoint - new Vector3 (5f, 0f, 5f);
 			}
-			//Debug.Log(hit.position.ToString() + " == " + fleePoint.ToString());*/
+//			Debug.Log (hit.position.ToString () + " == " + fleePoint.ToString ());
 			
-		}while(blocked && numBlock < 4);
-		
-		agent.SetDestination(fleePoint);
+		} while(blocked && numBlock < 4);
+
+//		Debug.Log ("Footman " + this.ToString () + " destination " + fleePoint.ToString () + ". Run from" + skeleton.transform.position.ToString () + ". Run to " + parent.target.transform.position.ToString ());
+//		Debug.DrawLine (gameObject.transform.position, fleePoint, Color.yellow);
+//		Debug.DrawLine (gameObject.transform.position, skeleton.transform.position, Color.red);
+//		Debug.DrawLine (gameObject.transform.position, parent.target.transform.position, Color.green);
+
+		agent.SetDestination (fleePoint);
 		//play run animation
-		parent.animHandler().RunAnim();
+		parent.animHandler ().RunAnim ();
 	}
 }
